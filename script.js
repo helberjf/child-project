7ebe891d805d4b23b0c03bbfab3ba23130ea40b3
Helber Soares <helberjf@gmail.com>
2026-09-07 14:06:19 -0300

    counting points

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const monstro = document.getElementById("monstro");
+const textoPontos = document.getElementById("pontos");
+
+let pontos = 0;
 
 monstro.addEventListener("click", function () {
-    alert("Você pegou o monstrinho!");
+
+    pontos = pontos + 1;
+
+    textoPontos.innerText = pontos;
+
 });
